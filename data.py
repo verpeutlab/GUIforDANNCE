ef81df4d7e9de06d532df7bed9c8fcb638b95154
verpeutlab <84207044+verpeutlab@users.noreply.github.com>
2021-07-27 20:26:36 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+#basic way to import data in python (i use a mac so it may be slightly different for your device)
 import scipy.io
+#change pathway for your own device 
 mat=scipy.io.loadmat('/Users/ishaan/Downloads/save_data_AVG0.mat')
 mat['pred']
