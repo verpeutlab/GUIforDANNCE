c0ba483b3d5209599422c922c7bd6cfbe6f65529
verpeutlab <84207044+verpeutlab@users.noreply.github.com>
2021-07-27 20:08:25 -0700

    Update frametoframesinglevideo.py

--- frametoframesinglevideo.py
+++ frametoframesinglevideo.py
@@ -1,13 +1,17 @@
+#necessary imports for this program
 import tkinter as tk 
 import threading
 import imageio
 from PIL import Image, ImageTk
 
+#this code only has one video, which is labeled and captured below 
 video_name = "/Users/ishaan/Desktop/angles/angle1.mp4"
 video = imageio.get_reader(video_name)
 
+#necessary for tkinter prgrams 
 root = tk.Tk()
 
+#creates canvas/area to display information
 canvas1 = tk.Canvas(root, width = 500, height = 400)
 canvas1.grid(row=0, column=0)
 entry1 = tk.Entry(root)
@@ -15,6 +19,7 @@ entry2 = tk.Entry(root)
 canvas1.create_window(100, 140, window=entry1)
 canvas1.create_window(300, 140, window=entry2)
 
+#start and end for frame numbers
 def stream(label,x1,x2):
 
     framef = int(x1)
@@ -27,11 +32,12 @@ def stream(label,x1,x2):
         label.config(image=frame_image)
         label.image = frame_image
         if frame == framet: break
-
+            
+#captures or gets the users entered frame numbers
 def showNumber ():
     x1 = entry1.get()
     x2 = entry2.get()
-
+#this code uses threading for the vide although there are other ways to stream the video 
     if __name__=="__main__":
         my_label = tk.Label(root)
         my_label.grid()
@@ -39,7 +45,9 @@ def showNumber ():
         thread.daemon = 1
         thread.start()
 
+#creates play video button
 button1 = tk.Button(text='Play Video', command=showNumber)
 canvas1.create_window(100,180, window=button1)
 
+#necessary to end tkinter program
 root.mainloop()
