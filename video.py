8965e3aef9271002047329af53431a709d87cfa4
verpeutlab <84207044+verpeutlab@users.noreply.github.com>
2021-07-27 20:22:44 -0700

    Update video.py

--- video.py
+++ video.py
@@ -1,11 +1,14 @@
+#necessary python function (not using tkinter)
 import cv2
 
+#captures the video
 cap = cv2.VideoCapture('/Users/ishaan/Downloads/0.mp4')
  
+# if the video is opened it plays or displays an error
 if (cap.isOpened()== False):
   print("Error opening video stream or file")
  
-
+#basically allows the video to play until a frame isn't there(it ends) or a frame doesn't work 
 while (cap.isOpened()):
     
     ret, frame = cap.read()
@@ -23,4 +26,5 @@ while (cap.isOpened()):
  
 cap.release()
 
+#allows you to close the program 
 cv2.destroyAllWindows()
